dacdd59d0e553c1865da94137cdc80a8ce6ccc44
Hugo LOPEZ <hugo.lopez002@gmail.com>
2022-10-28 00:59:58 +0200

    add react components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me3.png'
+import HeaderSocials from './HeaderSocials'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Hugo Lopez</h1>
+        <h5 className="text-light">Web Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="that's me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>scroll down</a>
+      </div>
+    
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/hugo-lopez-508388224/" rel="noreferrer" target="_blank"><BsLinkedin /> </a>
+        <a href="https://github.com/HugoLOPEZ64" rel="noreferrer" target="_blank"> <FaGithub /> </a>
+        <a href="https://www.linkedin.com/in/hugo-lopez-508388224/" rel="noreferrer" target="_blank"><BsLinkedin /> </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
