31ea882d7ae5b8ede8f1cd8d8e375f62debe2fa7
Hugo LOPEZ <hugo.lopez002@gmail.com>
2022-11-03 16:04:49 +0100

    add swiperjs

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -5,26 +5,38 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+// import Swiper core and required modules
+import { Pagination, Navigation } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+
+
 const data = [
   {
     avatar: AVTR1,
     name: 'Jean Bonbeur',
-    review: 'absolutely amazing experience working with hugo!'
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tortor urna, vehicula eget dictum sit amet, vestibulum quis nisl. Maecenas scelerisque augue nisl, tincidunt consequat nunc viverra id. Vestibulum efficitur'
   },
   {
     avatar: AVTR2,
     name: 'Lary Golade',
-    review: 'Hugo has the softest skills i have ever seen'
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tortor urna, vehicula eget dictum sit amet, vestibulum quis nisl. Maecenas scelerisque augue nisl, tincidunt consequat nunc viverra id. Vestibulum efficitur'
   },
   {
     avatar: AVTR3,
     name: 'Zinédine Zidane',
-    review: 'As well as being an incredible web developer, Hugo is also a terrific football player'
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tortor urna, vehicula eget dictum sit amet, vestibulum quis nisl. Maecenas scelerisque augue nisl, tincidunt consequat nunc viverra id. Vestibulum efficitur'
   },
   {
     avatar: AVTR4,
     name: 'Denis Brogniart',
-    review: 'AH'
+    review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tortor urna, vehicula eget dictum sit amet, vestibulum quis nisl. Maecenas scelerisque augue nisl, tincidunt consequat nunc viverra id. Vestibulum efficitur'
   },
 ]
 
@@ -34,22 +46,27 @@ const Testimonials = () => {
       <h5>What they think of me</h5>
       <h2>Review about my work</h2>
 
-      <div className="container testimonials__container">
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination, Navigation]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}>
         {
           data.map(({avatar, name, review}, index) => {
             return (
-              <article key={index} className="testimonial">
+              <SwiperSlide key={index} className="testimonial">
           <div className="client__avatar">
             <img src={avatar} alt=""/>
           </div>
           <h5 className='client__name'>{name}</h5>
                 <small className='client__review'>{review}</small>
-        </article>
+        </SwiperSlide>
           
             )
           })
         }
-      </div>   
+      </Swiper>   
     </section>
   )
 }
